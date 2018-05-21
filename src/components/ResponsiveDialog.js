fa052ba20d533c0e55471f77fe948d982c2902d4
Damian Wisniewski <wisniad@gmail.com>
2018-05-21 12:22:22 +0200

    Small styling progress

--- src/components/ResponsiveDialog.js
+++ src/components/ResponsiveDialog.js
@@ -7,6 +7,8 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import withMobileDialog from '@material-ui/core/withMobileDialog';
+import ReactTooltip from 'react-tooltip'
+import Grid from '@material-ui/core/Grid';
 
 class ResponsiveDialog extends React.Component {
     state = {
@@ -14,31 +16,45 @@ class ResponsiveDialog extends React.Component {
     };
 
     handleClickOpen = () => {
-        this.setState({ open: true });
-        console.log('this are props in component',this.props)
+        this.setState({open: true});
+        console.log('this are props in component', this.props)
     };
 
     handleClose = () => {
-        this.setState({ open: false });
+        this.setState({open: false});
     };
 
     render() {
-        const { fullScreen } = this.props;
+        const {fullScreen} = this.props;
 
         return (
             <div>
-                <Button onClick={this.handleClickOpen}>Read more</Button>
+                <a data-tip data-for='readMore' onClick={this.handleClickOpen}>
+                    <div className="gallery__item">
+                        {this.props.picture}
+                        <h3 className="divContainer__centered">{this.props.name}</h3>
+                    </div>
+                </a>
+                <ReactTooltip id='readMore' place="top" type="light"
+                              effect="float">
+                    <p><strong>Click</strong> to read more about</p>
+                </ReactTooltip>
+
                 <Dialog
                     fullScreen={fullScreen}
                     open={this.state.open}
                     onClose={this.handleClose}
                     aria-labelledby="responsive-dialog-title"
                 >
-                    <DialogTitle id="responsive-dialog-title">{`Details about ${this.props.name}`}</DialogTitle>
+                    <DialogTitle id="responsive-dialog-title"><h3>{`Details about ${this.props.name}`}</h3></DialogTitle>
                     <DialogContent>
-                       {this.props.picture}
                         <DialogContentText>
-                            {this.props.text}
+                            <Grid item xs={12} className="divContainer__centered">
+                                <a href={this.props.link} target="_blank">{this.props.picture}</a>
+                            </Grid>
+                            <Grid item xs={12} className="divContainer__centered">
+                                {this.props.text}
+                            </Grid>
                         </DialogContentText>
                     </DialogContent>
                     <DialogActions>
